85c8eb116dabed11164b08ffaa577a7fcf8d4198
Sofiane Elgarni <sofianeelgarni@MacBook-Pro-de-Sofiane.local>
2022-03-03 16:08:45 +0100

    Sofiane

---
+++ about.js
@@ -0,0 +1,27 @@
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  animate_text();
+});
+
+function animate_text() 
+{
+  let delay = 100,
+      delay_start = 0,
+      contents,
+      letters;
+
+  document.querySelectorAll(".animate-text").forEach(function (elem) {
+    contents = elem.textContent.trim();
+    elem.textContent = "";
+    letters = contents.split("");
+    elem.style.visibility = 'visible';
+
+    letters.forEach(function (letter, index_1) {
+        setTimeout(function () {
+        
+          elem.textContent += letter;
+      }, delay_start + delay * index_1);
+  });    
+  delay_start += delay * letters.length;
+});
+}
